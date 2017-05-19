366935d5f6fe2bc0e01bd5e64d765ca01b35a397
Jonatan Rinckus <jonatan@loop3.com.br>
2017-05-19 10:24:43 -0300

    Some

---
+++ RouteIssue/Controllers/Api/SomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteIssue.Controllers.Api
+{
+	[Route("Api/Home")]
+    public class SomeController : Controller
+	{
+		public IActionResult Get()
+	    {
+		    return Ok("Something");
+	    }
+
+		[HttpPost]
+		public IActionResult Create(object something)
+		{
+			return Ok();
+		}
+	}
+}
+
---
+++ RouteIssue/Controllers/SomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RouteIssue.Controllers
+{
+    public class SomeController : Controller
+    {
+	    public IActionResult Create()
+	    {
+		    return View();
+	    }
+
+	}
+}
